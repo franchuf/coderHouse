4d5fddf9ed8a39be812fbc1854c6d397bd734967
Francisco Faner <franchuf@gmail.com>
2021-10-16 00:44:14 -0300

    ultimas correcciones, trabajo aprobado

--- clase03/server.js
+++ clase03/server.js
@@ -11,17 +11,17 @@ prueba.save({a:1})
 app.get("/productos", function(req, res){
     let nuevoArray = JSON.parse (fs.readFileSync("./productos.txt" ,'utf-8'))
     console.log(nuevoArray);
-    return nuevoArray //si la consigna implica devolver los productos cuando se corre la funcion
+    res.json(nuevoArray)  //si la consigna implica devolver los productos cuando se corre la funcion
 })
 app.get("/productoRandom", function(req, res){
     let nuevoArray = JSON.parse(fs.readFileSync("./productos.txt",'utf-8'))
     var x = Math.floor(Math.random()*(nuevoArray.length));
     console.log(nuevoArray[x]);
-    return nuevoArray[x]
+    res.json(nuevoArray[x])
 })
 
-app.listen (8041, function(){
-    console.log("server running on port 8080")
+const listener = app.listen (8080, function(){
+    console.log('listening on port ' + listener.address().port);
 })
 
 const cont1 = new modulo.Contenedor("prueba")
