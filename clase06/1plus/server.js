118e5f01657a8ff6dd3fe81d93f4018b04002632
franchuf <franchuf@gmail.com>
2021-11-08 07:18:42 -0300

    practica de finde

--- clase06/1plus/server.js
+++ clase06/1plus/server.js
@@ -1,15 +1,30 @@
-const express = require ("express");
-const bodyParser = require("body-parser")
-const app = express ();
+const express = require('express')
+const { Server: HttpServer } = require('http')
+const { Server: IOServer } = require('socket.io')
 
-// app.use(express.static('public'))
-app.use('/static', express.static('public'))
+const app = express()
+const httpServer = new HttpServer(app)
+const io = new IOServer(httpServer)
 
+const mensajes = []
 
+app.use(express.static('public'))
+
+io.on('connection', socket => {
+    console.log('Nuevo cliente conectado!')
+
+    /* Envio los mensajes al cliente que se conectó */
+    socket.emit('mensajes', mensajes)
+
+    /* Escucho los mensajes enviado por el cliente y se los propago a todos */
+    socket.on('mensaje', data => {
+        mensajes.push({ socketid: socket.id, mensaje: data })
+        io.sockets.emit('mensajes', mensajes)
+    })
+})
 
 const PORT = 8080
-const server = app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${server.address().port}`)
+const connectedServer = httpServer.listen(PORT, function () {
+    console.log(`Servidor Http con Websockets escuchando en el puerto ${connectedServer.address().port}`)
 })
-server.on('error', error => console.log(`Error en servidor ${error}`))
-
+connectedServer.on('error', error => console.log(`Error en servidor ${error}`))
